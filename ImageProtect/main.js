34a6c52a4248c29db5defb45a7faf3fc27c86730
Mesud Mujanovic <mesud9494@gmail.com>
2023-03-25 15:13:26 +0100

    m

--- ImageProtect/main.js
+++ ImageProtect/main.js
@@ -4,29 +4,47 @@ let inputPassword = document.querySelector('#password');
 let btn = document.querySelector('.btn');
 let aHref = document.querySelector('.aHref');
 let background = document.querySelector('.background');
+
 let password = 'mesud123';
+btn.addEventListener('click', checkPassword);
+inputPassword.addEventListener('input',resetInputPassword);
+inputEmail.addEventListener('input',resetEmailPassword);
 
-btn.addEventListener('click', () => {
-    let passValue = inputPassword.value;
-    let length = passValue.length;
-    if(length <= 8){
-        checkPassword(passValue);
-    }
-});
-
-function checkPassword(passValue){
-
-    if(password === passValue){
-        console.log(password);
-        console.log(passValue);
-        showImage();
-    }else{
-        inputPassword.style.border = "1px solid red";
-    }
+function checkPassword() {
+    let valPass = inputPassword.value;
+    let valEmail = inputEmail.value;
+    let atPosition = valEmail.indexOf("@");
+    if (valPass === password) {
+        console.log("tacna passs");
+        if (valEmail.includes("@") && valEmail.includes(".") && valEmail.endsWith("com")
+            && atPosition > 2 && atPosition < valEmail.length - 3) {
+            showImage();
+        } else {
+            errorEmail();
+        };
+    } else {
+        errorPasword();       
+    };
 };
 
-function showImage(){
-   card.style.display = "none";
-   background.style.filter = "blur(0px)";
-   aHref.style.display = "block";
+function resetInputPassword() {
+    inputPassword.style.border = "1px solid #333";
+};
+
+function resetEmailPassword () {
+    inputEmail.style.border = "1px solid #333";
+};
+
+function showImage() {
+    card.style.display = "none";
+    background.style.filter = "blur(0px)";
+    aHref.style.display = "block";
+};
+
+function errorPasword() {
+    inputPassword.style.border = "1px solid red";
+};
+
+function errorEmail() {
+    inputEmail.style.border = "1px solid red";
 };
